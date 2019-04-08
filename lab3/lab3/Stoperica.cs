d52f62de4733730c77439fefb600cf2482241607
mbesten <mbesten97@gmail.com>
2019-04-08 10:14:27 +0200

    Zadatak labosi3

---
+++ lab3/lab3/Stoperica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lab3
+{
+    class Stoperica
+    {
+        static Stopwatch Mjerac = new Stopwatch();
+        public static void ZapocniMjerenje()
+        {
+            Mjerac.Start();
+        }
+
+        public static TimeSpan VremenskiInterval()
+        {
+            TimeSpan vrijeme = Mjerac.Elapsed;
+
+            return vrijeme;
+        }
+    }
+}
